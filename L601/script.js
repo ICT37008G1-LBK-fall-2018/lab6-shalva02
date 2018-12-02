6607147311201e6be97c578d6b7a264c15dc2dde
shalva02 <zaqaidzes@gmail.com>
2018-12-02 21:42:46 +0400

    lab6

--- L601/script.js
+++ L601/script.js
@@ -3,4 +3,4 @@ for (var i = 0; i < document.getElementsByTagName('div').length; i++) {
     if (document.getElementsByTagName('div')[i].getAttribute('data-student-id') == x) {
         document.getElementsByTagName('div')[i].style.background = 'red'
     }
-}
+} 
