1742dac53927b99a836c5a3303a6ca82995f1440
shalva02 <zaqaidzes@gmail.com>
2018-12-02 19:38:27 +0400

    lab6

--- L602/script.js
+++ L602/script.js
@@ -1,13 +1,7 @@
 var first = document.getElementById('first');
 var second = document.getElementById('second');
 
-function insertAfter(firstElement, secondElement) {
-    var container = document.getElementById('container');
-    if (container.lastChild == secondElement) {
-        container.appendChild(firstElement);
-    } else {
-        container.insertBefore(firstElement, secondElement.nextSibling);
-    }
-
+function insertAfter(newElem, oldElem) {
+    oldElem.parentNode.insertBefore(newElem, oldElem.nextSibling)
 }
 insertAfter(first, second)
